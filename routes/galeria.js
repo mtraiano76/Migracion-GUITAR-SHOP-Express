36cf336d49bae61fc2c93dad166274e338e08d7a
Miguel Traiano <92335885+mtraiano76@users.noreply.github.com>
2022-07-04 10:04:17 -0300

    Formulario de contacto

--- routes/galeria.js
+++ routes/galeria.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('galeria', { title: 'Express' });
+  res.render('galeria');
 });
 
 module.exports = router;
