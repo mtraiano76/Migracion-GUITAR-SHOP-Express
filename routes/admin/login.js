af194c3a983e5ae0c68eff81294da97f79840c52
Miguel Traiano <92335885+mtraiano76@users.noreply.github.com>
2022-07-11 12:06:22 -0300

    Saludo del login

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -24,7 +24,7 @@ router.post("/", async function(req,res,next){
     if(data != undefined){
 
       req.session.id_usuario = data.id; // 1
-      req.session.nombre = data.usuario; // Miguel
+      req.session.nombre = data.nombre; // Miguel
       
       res.redirect("/admin/novedades")
     }else{
